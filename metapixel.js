82add1cfc60604f456590d986c2290ae298b8bf2
George Madrid <gmadrid@gmail.com>
2021-01-26 08:56:53 -0500

    Got it working!

--- metapixel.js
+++ metapixel.js
@@ -1,6 +1,6 @@
 import init, { run_metapixel_app } from './pkg/metapixel.js';
 async function main() {
-   await init('/pkg/twopattern_bg.wasm');
+   await init('/pkg/metapixel_bg.wasm');
    run_metapixel_app();
 }
 main()
